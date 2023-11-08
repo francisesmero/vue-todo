bdcec3ff201ab5ab1b96be579d3c1deb50ef31b3
Francis Esmero <francis.esmero@gmail.com>
2023-11-08 16:51:18 +0800

    create task list component

--- src/components/heroBanner.vue
+++ src/components/heroBanner.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="my-8 bg-[#98c0f7] px-6 py-6 flex flex-row rounded-[25px]">
-           <h1 class="font-bold text-white basis-1/2">
+    <div class="my-4 p-8 flex flex-row rounded-[15px] bg-gradient-primary">
+           <h1 class="font-bold text-xl text-white basis-1/2 leading-6">
             Manage your <br> time well
            </h1>
 
