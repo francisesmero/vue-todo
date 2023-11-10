2dd2551d7e168630c3969f667052cc9e83679a6d
Francis Esmero <francis.esmero@gmail.com>
2023-11-10 18:50:39 +0800

    create modal for adding todo task

--- src/App.vue
+++ src/App.vue
@@ -4,36 +4,54 @@ import profile from './components/profile.vue';
 import heroBanner from './components/heroBanner.vue'
 import taskCategories from './components/taskCategories.vue'
 import taskList from './components/taskList.vue'
+
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('');
+const input_category = ref(null)
+
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return  a.createdAt - b.createdAt
+}))
+
+
+
+
 </script>
 
 <template>
+  
    <div class="container mx-auto">
+    
     <div class="flex justify-between items-center">
     <button class="text-gray-600 p-1 rounded-[10px] border-solid border-2 border-[#f4f4f4] bg-white P-2">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
       <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
     </svg>
-
     </button>
 
     <div class="flex items-center space-x-4">
-      <img src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="Avatar" class="h-10 w-10 rounded-full border-solid border-2 border-[#f4f4f4]">
+      <img src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="Avatar" class="h-9 w-9 rounded-full border-solid border-2 border-[#e9e9e9]">
     </div>
   </div>
 
     <heroBanner/>
-
-    <h2 class="text-neutral-500 font-bold">Categories</h2>
     <taskCategories/>
-    
-    <h2 class="text-neutral-500 font-bold my-4">You have 10 task for today</h2>
     <taskList/>
+
+
+    
+
+  
 </div>
 
   
 
+
 </template>
 
 <style scoped>
-
 </style>
