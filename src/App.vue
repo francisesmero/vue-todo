ff06358bc716a4a768c93da8f27ebd581e16fba0
Francis Esmero <francis.esmero@gmail.com>
2023-11-22 11:25:02 +0800

    add timepicker for creating todo

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,46 @@
 <script setup>
-import sideNavigation from './components/sideNavigation.vue'
-import profile from './components/profile.vue';
-import heroBanner from './components/heroBanner.vue'
-import taskCategories from './components/taskCategories.vue'
-import taskList from './components/taskList.vue'
-
-
-
-
-
-
+import sideNavigation from "./components/sideNavigation.vue";
+import profile from "./components/profile.vue";
+import heroBanner from "./components/heroBanner.vue";
+import taskCategories from "./components/taskCategories.vue";
+import taskList from "./components/taskList.vue";
 </script>
 
 <template>
-  
-   <div class="container mx-auto">
-    
+  <div class="container mx-auto">
     <div class="flex justify-between items-center">
-    <button class="text-gray-600 p-1 rounded-[10px] border-solid border-2 border-[#f4f4f4] bg-white P-2">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-    </svg>
-    </button>
+      <button
+        class="text-gray-600 p-1 rounded-[10px] border-solid border-2 border-[#f4f4f4] bg-white P-2"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
 
-    <div class="flex items-center space-x-4">
-      <img src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="Avatar" class="h-9 w-9 rounded-full border-solid border-2 border-[#e9e9e9]">
+      <div class="flex items-center space-x-4">
+        <img
+          src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          alt="Avatar"
+          class="h-9 w-9 rounded-full border-solid border-2 border-[#e9e9e9]"
+        />
+      </div>
     </div>
+
+    <heroBanner />
+    <taskCategories />
+    <taskList />
   </div>
-
-    <heroBanner/>
-    <taskCategories/>
-    <taskList/>
-
-
-    
-
-  
-</div>
-
-  
-
-
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/taskList.vue
+++ src/components/taskList.vue
@@ -1,54 +1,60 @@
 <template>
-  <h2 class="text-neutral-500 font-bold mb-4">You have {{ totalItems }} task for today</h2>
+  <h2 class="text-neutral-500 font-bold mb-4">
+    You have {{ totalItems }} task for today
+  </h2>
 
-<div class="card-todo flex flex-row gap-x-2" v-for="todo in todos" :key="todo.id">
-<div class="w-full card-item bg-white p-4 flex flex-row justify-between items-center rounded-[15px] my-2">
-    <div class="flex items-center space-x-4">
-        <span class="rounded-full p-2 w-2 h-2" 
-            :class="{
-                'bg-red-500': todo.todoCategory === 'health',
-                'bg-gradient-primary': todo.todoCategory === 'work',
-                'bg-orange-400': todo.todoCategory === 'personal',
-                'bg-green-300': todo.todoCategory === 'shopping'
-              }"
-            ></span>
-        <p class="text-neutral-600 ml-6">{{todo.todoItem}}</p>
+  <div
+    class="card-todo flex flex-row gap-x-2"
+    v-for="todo in sortedTodos"
+    :key="todo.id"
+  >
+    <div
+      class="w-full card-item bg-white p-4 flex flex-row justify-between items-center rounded-[15px] my-2"
+    >
+      <div class="flex items-center space-x-4">
+        <span
+          class="rounded-full p-2 w-2 h-2"
+          :class="{
+            'bg-red-500': todo.todoCategory === 'health',
+            'bg-gradient-primary': todo.todoCategory === 'work',
+            'bg-orange-400': todo.todoCategory === 'personal',
+            'bg-green-300': todo.todoCategory === 'shopping',
+          }"
+        ></span>
+        <p class="text-neutral-600 ml-6">{{ todo.todoItem }}</p>
+      </div>
+
+      <div class="flex items-center space-x-4">
+        <p class="text-neutral-500">{{ todo.todoTime }}</p>
+      </div>
     </div>
-    
-    <div class="flex items-center space-x-4">
-    <p class="text-neutral-500">{{todo.todoTime}}</p>
-    </div>
-</div>
-</div>
-
-
-
-
-    
-
-
+  </div>
 </template>
 
 <script setup>
-
-import {ref, onMounted, computed } from 'vue';
-import data from '../../data.json'
+import { ref, onMounted, computed } from "vue";
+import data from "../../data.json";
 
 const todos = ref([]);
 
-onMounted(()=> {
+onMounted(() => {
+  todos.value = data.todos;
+});
 
-    todos.value = data.todos;
+const sortedTodos = computed(() => {
+  return todos.value.slice().sort((a, b) => {
+    const timeA = a.todoTime.split(":").map(Number);
+    const timeB = b.todoTime.split(":").map(Number);
 
-})
+    if (timeA[0] !== timeB[0]) {
+      return timeA[0] - timeB[0];
+    }
 
+    return timeA[1] - timeB[1];
+  });
+});
 
-
-const totalItems = computed(() => todos.value.length);
-
-
+const totalItems = computed(() => sortedTodos.value.length);
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
